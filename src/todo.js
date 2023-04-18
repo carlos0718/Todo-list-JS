d4c7aa5b052b70e9ee24f53a8138811c2572c678
c.sipiran07@gmail.com <carlos0718>
2023-04-17 19:34:11 -0500

    adding file src for class TODO

---
+++ src/todo.js
@@ -0,0 +1,34 @@
+class Todo {
+	constructor() {
+		this.name = name;
+		this.id = id;
+	}
+
+	saveToStorage(name, id) {
+		const task = new Todo();
+		task.id = id;
+		task.name = name;
+
+		taskStorage.push(task);
+		console.log(task.name);
+		localStorage.setItem("todo", JSON.stringify(taskStorage));
+
+		addTask.innerHTML = "";
+		this.readFromStorage();
+	}
+
+	readFromStorage() {
+		if (taskStorage.length > 0) {
+			taskStorage.forEach((task) => {
+				addTask.innerHTML += `<li class="py-4 flex justify-between items-center" id=${task.id}>
+							<span class="text-lg font-mediun text-blue-500">${task.name}</span>
+							<div class="flex">
+								<button class="text-white rounded-md px-2 py-1 mr-2">✅</button>
+								<button class="text-white rounded-md px-2 py-1" onclick=deleteTaskFromStorage(${task.id})>❌</button>
+							</div>
+						</li>
+				`;
+			});
+		}
+	}
+}
